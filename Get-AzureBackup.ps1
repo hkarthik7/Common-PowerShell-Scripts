fc386e4462d4c0f63bb36ff3fdb0688716797b24
hkarthik7 <harish.karthic@gmail.com>
2019-11-26 10:47:20 +0000

    Initial Commit

--- Get-AzureBackup.ps1
+++ Get-AzureBackup.ps1
@@ -1,129 +1,131 @@
 ﻿Function Get-AzureBackup {
 
-<#
-    
-    .SYNOPSIS
-    Gets VM name Backup information from Azure portal.
-
-    .DESCRIPTION
-    This script is designed to extract complete backup information for given subscription.
-
-    .PARAMETER
-    .STRING Array. Subscriptions. Provide the subscription name.
-
-    .PARAMETER
-    .STRING. ExportPath. Provide the filepath to export information to a csv file name.
-
-    .PARAMETER
-    .STRING. Logpath. Provide the filepath to track the logs.
-
-    .EXAMPLE
-    Get-AzureBackup -Subscription ("Subscription Name","Subscription Name1") -ExportPath "C:\Temp" -LogPath "C:\Temp" -Verbose
-
-    .NOTES
-	    Author						Version			Date			Notes
-		--------------------------------------------------------------------------------------------------------------------
-		harish.b.karthic		    v1.0			12/09/2019		Initial script
-
-#>
-
-[CmdletBinding()]
-Param(
-    
-    [Parameter(Mandatory=$true)]
-    [ValidateNotNullorEmpty()]
-    [String[]]$Subscriptions,
-
-    [Parameter(Mandatory=$true)]
-    [String]$ExportPath,
-
-    [Parameter(Mandatory=$true)]
-    [String]$LogPath
-
-)
-
-begin {
-
-    #initialize function variables
-    $functionName = $MyInvocation.MyCommand.Name
-    $Logfile = $LogPath + "\Backup_$($Subscription)_$(Get-Date -Format dd-MM-yyyy).log"
-    $ExportPath = $ExportPath.TrimEnd("\")
-    $ExportPath = $ExportPath + "\Backup_$($Subscription)_$(Get-Date -Format dd-MM-yyyy).csv"
-
-    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Begin function.."
-    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
-
-    $CSV = @()
-
-}#begin
-
-process {
-
-    try {
+    <#
         
-        foreach($Subscription in $Subscriptions) {
+        .SYNOPSIS
+        Gets VM name Backup information from Azure portal.
 
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Selecting the given Subscription $($Subscription).."
+        .DESCRIPTION
+        This script is designed to extract complete backup information for given subscription.
+
+        .PARAMETER
+        .STRING Array. Subscriptions. Provide the subscription name.
+
+        .PARAMETER
+        .STRING. ExportPath. Provide the filepath to export information to a csv file name.
+
+        .PARAMETER
+        .STRING. Logpath. Provide the filepath to track the logs.
+
+        .EXAMPLE
+        Get-AzureBackup -Subscription ("Subscription Name","Subscription Name1") -ExportPath "C:\Temp" -LogPath "C:\Temp" -Verbose
+
+        .NOTES
+            Author						Version			Date			Notes
+            --------------------------------------------------------------------------------------------------------------------
+            harish.b.karthic		    v1.0			12/09/2019		Initial script
+
+    #>
+
+    [CmdletBinding()]
+    Param(
+        
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullorEmpty()]
+        [String[]]$Subscriptions,
+
+        [Parameter(Mandatory=$true)]
+        [String]$ExportPath,
+
+        [Parameter(Mandatory=$true)]
+        [String]$LogPath
+
+    )
+
+    begin {
+
+        #initialize function variables
+        $functionName = $MyInvocation.MyCommand.Name
+        $Logfile = $LogPath + "\Backup_$($Subscription)_$(Get-Date -Format dd-MM-yyyy).log"
+        $ExportPath = $ExportPath.TrimEnd("\")
+        $ExportPath = $ExportPath + "\Backup_$($Subscription)_$(Get-Date -Format dd-MM-yyyy).csv"
+
+        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Begin function.."
         Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
-        Select-AzSubscription -Subscription $Subscription | Out-Null
+        $CSV = @()
 
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching Vault details.."
-        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+    }#begin
 
-        $Vaults = Get-AzRecoveryServicesVault
+    process {
 
-        Foreach($Vault in $Vaults) {
+        try {
             
-            $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Working with Vault $($Vault.Name).."
-            Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+            foreach($Subscription in $Subscriptions) {
 
-            #Set current vault from which the details has to be fetched
-            Set-AzRecoveryServicesVaultContext -Vault $Vault -WarningAction SilentlyContinue
-            $Containers = Get-AzRecoveryServicesBackupContainer -ContainerType "AzureVM" -Status "Registered.."
+                $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Selecting the given Subscription $($Subscription).."
+                Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
-            $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup information from available containers.."
-            Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+                Select-AzSubscription -Subscription $Subscription | Out-Null
 
-                Foreach($Container in $Containers) {
-            
-            $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup information for $($Container.FriendlyName).."
-            Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+                $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching Vault details.."
+                Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+
+                $Vaults = Get-AzRecoveryServicesVault
+
+                Foreach($Vault in $Vaults) {
                     
-                    $BackupState = Get-AzRecoveryServicesBackupItem -Container $Container -WorkloadType AzureVM
-                    $BackupVMName = ($BackupState.VirtualMachineId).Split("/")[-1]
-                    $Backupenabled = If($BackupState.ProtectionState -eq "Protected"){"Enabled"} else {"Disabled"}
-                    $BackupPolicy = $BackupState.ProtectionPolicyName
+                    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Working with Vault $($Vault.Name).."
+                    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
-                    $Hash = [PSCustomObject]@{
-        
-                        BackupVMName = $BackupVMName
-                        Backupenabled = $Backupenabled
-                        BackupPolicy = $BackupPolicy
-                    }
-                    $CSV += $Hash
+                    #Set current vault from which the details has to be fetched
+                    Set-AzRecoveryServicesVaultContext -Vault $Vault -WarningAction SilentlyContinue
+                    $Containers = Get-AzRecoveryServicesBackupContainer -ContainerType "AzureVM" -Status "Registered.."
 
-        }
+                    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup information from available containers.."
+                    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+
+                    Foreach($Container in $Containers) {
+                    
+                        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup information for $($Container.FriendlyName).."
+                        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+                        
+                        $BackupState = Get-AzRecoveryServicesBackupItem -Container $Container -WorkloadType AzureVM
+                        $BackupVMName = ($BackupState.VirtualMachineId).Split("/")[-1]
+                        $Backupenabled = If($BackupState.ProtectionState -eq "Protected"){"Enabled"} else {"Disabled"}
+                        $BackupPolicy = $BackupState.ProtectionPolicyName
+
+                        $Hash = [PSCustomObject]@{
             
-    
-}#foreach
+                            BackupVMName = $BackupVMName
+                            Backupenabled = $Backupenabled
+                            BackupPolicy = $BackupPolicy
+                        }
+                        $CSV += $Hash
 
-}#foreach
+                    }
+
+                }#foreach
+
+            }#foreach
+
             $CSV | Export-Csv $ExportPath -NoTypeInformation
 
             $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Backup information is fetched and saved to $($ExportPath).."
             Write-Verbose $Message ; $Message | Out-File -Append $LogFile
-} Catch{
-        Write-Verbose " [$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).."
-        "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).." | Out-File -Append $LogFile
+
+        } 
+        Catch{
+            Write-Verbose " [$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).."
+            "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).." | Out-File -Append $LogFile
         }
 
-}#process
+    }#process
 
-end {
+    end {
 
-            $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : End Function.."
-            Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : End Function.."
+        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
     }
 
--- Get-AzureBackupStatus.ps1
+++ Get-AzureBackupStatus.ps1
@@ -1,139 +1,140 @@
 ﻿Function Get-AzureBackupStatus {
 
-<#
-    
-    .SYNOPSIS
-    Get backup job status for given hours.
-
-    .DESCRIPTION
-    This script is designed to run and fetch the information of backup job status for given hours.
-
-    .PARAMETER
-    .STRING. Subscription. Provide the subscription name.
-
-    .PARAMETER
-    .STRING. OutputPath. Provide the filepath to export information to a csv file name.
-
-    .PARAMETER
-    .Int. LastBackupHours. Provide the number of hours for which the backup details has to be fetched.
-
-    .PARAMETER
-    .STRING. Logpath. Provide the filepath to track the logs.
-
-    .EXAMPLE
-    Get-AzureBackupStatus -Subscriptions "Subscription Name","Subscription Name 2" -LastBackupHours 48 -OutputPath "C:\Temp" -LogPath "C:\Temp" -Verbose
-
-    .NOTES
-	    Author						Version			Date			Notes
-		--------------------------------------------------------------------------------------------------------------------
-		harish.b.karthic		    v1.0			12/09/2019		Initial script
-
-#>
-
-[CmdletBinding()]
-Param(
-
-    [Parameter(Mandatory=$true)]
-    [ValidateNotnullorEmpty()]
-    [String[]]$Subscriptions,
-
-    [Parameter(Mandatory=$true)]
-    [ValidateNotnullorEmpty()]
-    [Int]$LastBackupHours,
-
-    [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$OutPutPath,
-
-    [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$LogPath
-
-)
-
-Begin {
-    
-    #initialize function variables
-    $FunctionName = $MyInvocation.MyCommand.Name
-    $OutPutPath = $OutPutPath + "\Last$($LastBackupHours)HoursBackupDetails_$(Get-Date -Format dd-MM-yyyy).csv"
-    $Logfile = $LogPath + "\Last$($LastBackupHours)HoursBackupDetails_$(Get-Date -Format dd-MM-yyyy).log"
-    $CSV = @()
-
-    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Begin function.."
-    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
-
-}#Begin
-
-Process {
-
-    try {
+    <#
         
-        Foreach($Subscription in $Subscriptions) {
+        .SYNOPSIS
+        Get backup job status for given hours.
 
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Selecting Subscription $($Subscription) to work with.."
-        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+        .DESCRIPTION
+        This script is designed to run and fetch the information of backup job status for given hours.
 
-        #Select the given subscription
-        Select-AzSubscription -Subscription $Subscription | Out-Null
+        .PARAMETER
+        .STRING. Subscription. Provide the subscription name.
 
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching Vault information.."
-        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+        .PARAMETER
+        .STRING. OutputPath. Provide the filepath to export information to a csv file name.
 
-        #fetch information from all vaults
-        $Vaults = Get-AzRecoveryServicesVault
-        #$CSV = @()
-    
-        Foreach($Vault in $Vaults) {
+        .PARAMETER
+        .Int. LastBackupHours. Provide the number of hours for which the backup details has to be fetched.
+
+        .PARAMETER
+        .STRING. Logpath. Provide the filepath to track the logs.
+
+        .EXAMPLE
+        Get-AzureBackupStatus -Subscriptions "Subscription Name","Subscription Name 2" -LastBackupHours 48 -OutputPath "C:\Temp" -LogPath "C:\Temp" -Verbose
+
+        .NOTES
+        Author						Version			Date			Notes
+        --------------------------------------------------------------------------------------------------------------------
+        harish.b.karthic		    v1.0			12/09/2019		Initial script
+
+    #>
+
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotnullorEmpty()]
+        [String[]]$Subscriptions,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotnullorEmpty()]
+        [Int]$LastBackupHours,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]$OutPutPath,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_})]
+        [String]$LogPath
+
+    )
+
+    Begin {
         
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Working with Vault $($Vault.Name).."
+        #initialize function variables
+        $FunctionName = $MyInvocation.MyCommand.Name
+        $OutPutPath = $OutPutPath + "\Last$($LastBackupHours)HoursBackupDetails_$(Get-Date -Format dd-MM-yyyy).csv"
+        $Logfile = $LogPath + "\Last$($LastBackupHours)HoursBackupDetails_$(Get-Date -Format dd-MM-yyyy).log"
+        $CSV = @()
+
+        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Begin function.."
         Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
-            $BackupStatus = Get-AzRecoveryServicesBackupJob -VaultId $Vault.ID -From (Get-Date).AddHours(-$($LastBackupHours)).ToUniversalTime()
+    }#Begin
 
-            #Get required values
-            Foreach($Status in $BackupStatus) {
+    Process {
 
-         $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup details of $($Status.WorkloadName).."
-         Write-Verbose $Message ; $Message | Out-File -Append $LogFile   
-
-                $CSV += [PSCustomObject]@{
-                
-                    Name = $Status.WorkloadName
-                    Operation = $Status.Operation
-                    Status = $Status.Status
-                    StartTime = $Status.StartTime
-                    EndTime = $Status.EndTime
-                    Duration = $Status.Duration
-                    JobID = $Status.JobId
-                    Subscription = $Subscription
-                
-                }
-                
-            }
+        try {
             
-        }
+            Foreach($Subscription in $Subscriptions) {
 
-    }
-        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Exporting Backup details to $($OutPutPath).."
-        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
-    
-        $CSV | Export-Csv $OutPutPath -NoTypeInformation
-    
-    } Catch {
-    
-        Write-Verbose " [$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).."
-        "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)" | Out-File -Append $LogFile
-    
-    }#Catch
+                $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Selecting Subscription $($Subscription) to work with.."
+                Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+
+                #Select the given subscription
+                Select-AzSubscription -Subscription $Subscription | Out-Null
+
+                $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching Vault information.."
+                Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+
+                #fetch information from all vaults
+                $Vaults = Get-AzRecoveryServicesVault
         
-}#Process
+                Foreach($Vault in $Vaults) {
+                
+                    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Working with Vault $($Vault.Name).."
+                    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
 
-end {
+                    $BackupStatus = Get-AzRecoveryServicesBackupJob -VaultId $Vault.ID -From (Get-Date).AddHours(-$($LastBackupHours)).ToUniversalTime()
 
-    $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : End function.."
-    Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+                    #Get required values
+                    Foreach($Status in $BackupStatus) {
 
-}#end
+                        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Fetching backup details of $($Status.WorkloadName).."
+                        Write-Verbose $Message ; $Message | Out-File -Append $LogFile   
+
+                        $CSV += [PSCustomObject]@{
+                        
+                            Name = $Status.WorkloadName
+                            Operation = $Status.Operation
+                            Status = $Status.Status
+                            StartTime = $Status.StartTime
+                            EndTime = $Status.EndTime
+                            Duration = $Status.Duration
+                            JobID = $Status.JobId
+                            Subscription = $Subscription
+                        
+                        }
+                        
+                    }
+                    
+                }
+
+            }
+
+            $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Exporting Backup details to $($OutPutPath).."
+            Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+        
+            $CSV | Export-Csv $OutPutPath -NoTypeInformation
+        
+        }
+        Catch {
+        
+            Write-Verbose " [$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message).."
+            "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)" | Out-File -Append $LogFile
+        
+        }#Catch
+            
+    }#Process
+
+    end {
+
+        $Message = "[$(Get-Date -UFormat %Y/%m/%d_%H:%M:%S)] $functionName : End function.."
+        Write-Verbose $Message ; $Message | Out-File -Append $LogFile
+
+    }#end
 
 #EOF
 }
